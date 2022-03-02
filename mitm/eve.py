2038583c2cb2b677c1fc638e050cca4bd3096d1b
Arbnor Bregu <s1832263@faber.inf.ed.ac.uk>
2022-03-02 21:20:15 +0000

    update

--- mitm/eve.py
+++ mitm/eve.py
@@ -3,3 +3,8 @@ import os
 
 from common import *
 from const import *
+
+dialog = Dialog('print')
+player = os.path.basename(sys.argv[0]).split('.', 1)[0]
+socket, aes = setup(player, BUFFER_DIR, BUFFER_FILE_NAME)
+
