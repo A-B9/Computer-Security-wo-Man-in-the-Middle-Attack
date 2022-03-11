90e2c69e620bf4e618d48f9d9e514fb853966214
Arbnor Bregu <s1832263@fret.inf.ed.ac.uk>
2022-03-11 20:36:34 +0000

    Final Version, 100% Complete

--- mitm/eve.py
+++ mitm/eve.py
@@ -6,19 +6,16 @@ from const import *
 
 dialog = Dialog('print')
 player = os.path.basename(sys.argv[0]).split('.', 1)[0]#gets the name of the player from the command line input
-
-os.rename(BUFFER_DIR+BUFFER_FILE_NAME,BUFFER_DIR+"eveFile")
+os.rename(BUFFER_DIR+BUFFER_FILE_NAME,BUFFER_DIR+"eveFile")#rename the buffer file
 socket_bob, aes_bob = setup('bob', BUFFER_DIR, "eveFile")#connect to bobs socket
 socket_alice, aes_alice = setup('alice', BUFFER_DIR, BUFFER_FILE_NAME) #connect to alice socket 
 
 dialog.info('Connected to Alice as Bob.')
-#Bob and alice will speak to each other through these sockets,
-#I need to pass these messges from one socket to another.
 dialog.info('Connected to Bob as Alice')
 dialog.chat("Nice, getting there...")
 
-
-received_from_Bob = receive_and_decrypt(aes_alice, socket_alice)
+#Pretending to be Alice
+received_from_Bob = receive_and_decrypt(aes_alice, socket_alice)#decrypt the message that alice receives, using her socket and aes
 dialog.chat('Bob said: "{}"'.format(received_from_Bob))
 if os.path.basename(sys.argv[1]) == '--custom':
     CUSTOM_CHAT = True
@@ -31,11 +28,11 @@ else:
         to_send = received_from_Bob
     elif os.path.basename(sys.argv[1]) == '--break-heart':
         to_send = BAD_MSG['bob']
-encrypt_and_send(to_send, aes_bob, socket_bob)
+encrypt_and_send(to_send, aes_bob, socket_bob) #encrypt the message to be sent to Bobs socket
 dialog.info('Message sent!')
-######################################################################
 
-received_from_Alice = receive_and_decrypt(aes_bob, socket_bob)
+#Pretending to be Bob
+received_from_Alice = receive_and_decrypt(aes_bob, socket_bob)#decrypt the message that bob receives, using his socket and aes
 dialog.chat('Alice said: "{}"'.format(received_from_Alice))
 
 if CUSTOM_CHAT:
@@ -46,7 +43,7 @@ else:
         to_send = received_from_Alice
     elif os.path.basename(sys.argv[1]) == '--break-heart':
         to_send = BAD_MSG['alice']
-encrypt_and_send(to_send, aes_alice, socket_alice)
+encrypt_and_send(to_send, aes_alice, socket_alice)#encrypt the message and send to Alice
 dialog.info('Message sent!')
 
 tear_down(socket_bob, BUFFER_DIR, "eveFile")
