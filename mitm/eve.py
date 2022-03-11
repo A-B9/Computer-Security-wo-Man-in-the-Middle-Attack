830ca8c0d34fca7331d9471fd577c99225cdce5c
Arbnor Bregu <s1832263@fret.inf.ed.ac.uk>
2022-03-11 20:19:13 +0000

    successfully send messages across the sockets, cannot send the correct messages right now unfortunately

--- mitm/eve.py
+++ mitm/eve.py
@@ -5,31 +5,56 @@ from common import *
 from const import *
 
 dialog = Dialog('print')
-player = os.path.basename(sys.argv[0]).split('.', 1)[0]
-mode = os.path.basename(sys.argv[0]).split('--')[1]
+player = os.path.basename(sys.argv[0]).split('.', 1)[0]#gets the name of the player from the command line input
+mode = os.path.basename(sys.argv[1]).split('--')
 
-socket_alice, aes_alice = setup('bob', BUFFER_DIR, BUFFER_FILE_NAME) #connect to alice socket 
+os.rename(BUFFER_DIR+BUFFER_FILE_NAME,BUFFER_DIR+"eveFile")
+socket_bob, aes_bob = setup('bob', BUFFER_DIR, "eveFile")#connect to bobs socket
+socket_alice, aes_alice = setup('alice', BUFFER_DIR, BUFFER_FILE_NAME) #connect to alice socket 
+
 dialog.info('Connected to Alice as Bob.')
 #Bob and alice will speak to each other through these sockets,
 #I need to pass these messges from one socket to another.
-socket_bob, aes_bob = setup('alice', BUFFER_DIR, BUFFER_FILE_NAME)#connect to bobs socket
 dialog.info('Connected to Bob as Alice')
-
 dialog.chat("Nice, getting there...")
-received = receive_and_decrypt(aes_bob, socket_bob)
-dialog.chat('Bob said: "{}"'.format(received))
 
+
+received_from_Bob = receive_and_decrypt(aes_alice, socket_alice)
+dialog.chat('Bob said: "{}"'.format(received_from_Bob))
+
+to_send=''
 
 if CUSTOM_CHAT:
     dialog.prompt('please input message...')
     to_send = input()
 else:
     if mode == 'relay':
-        to_send = received
-    elif mode == 'break-heart'
+        to_send = received_from_Bob
+    elif mode == 'break-heart':
         to_send = BAD_MSG['bob']
+encrypt_and_send(to_send, aes_bob, socket_bob)
+dialog.info('Message sent!')
+
+
+######################################################################
+
+
+received_from_Alice = receive_and_decrypt(aes_bob, socket_bob)
+dialog.chat('Alice said: "{}"'.format(received_from_Alice))
+
+to_send = ""
+if CUSTOM_CHAT:
+    dialog.prompt('please input message...')
+    to_send = input()
+else:
+    if mode == 'relay':
+        to_send = received_from_Alice
+    elif mode == 'break-heart':
+        to_send = BAD_MSG['alice']
 encrypt_and_send(to_send, aes_alice, socket_alice)
 dialog.info('Message sent!')
 
+
+tear_down(socket_bob, BUFFER_DIR, "eveFile")
 tear_down(socket_alice, BUFFER_DIR, BUFFER_FILE_NAME)
-tear_down(socket_bob, BUFFER_DIR, BUFFER_FILE_NAME)
+
